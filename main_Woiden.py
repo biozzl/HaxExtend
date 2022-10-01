b9b2bea9581d5ce18aabcc0acbdb3a8ff3b62337
AllenZ <biozzl@gmail.com>
2022-10-01 21:48:48 +0800

    Update main_Woiden.py

--- main_Woiden.py
+++ main_Woiden.py
@@ -25,8 +25,8 @@ urlExtendRenew = 'https://woiden.id/vps-renew/'
 SpeechToTextURL = 'https://speech-to-text-demo.ng.bluemix.net/'
 
 # secret
-USERNAMEWOIDEN = os.environ['USERNAMEWOIDEN']
-PASSWORDWOIDEN = os.environ['PASSWORDWOIDEN']
+USERNAMEWOIDEN = ${{ secrets.USERNAMEWOIDEN }}
+PASSWORDWOIDEN = ${{ secrets.PASSWORDWOIDEN }}
 try:
     BARKKEY = os.environ['BARKKEY']
     barkKey = 1
